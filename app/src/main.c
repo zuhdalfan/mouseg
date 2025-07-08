46615878ad0e1c13b471027f83f05f4028fed13b
zuhdi <m.zuhdi@willowmore.com.sg>
2025-07-08 19:23:06 +0700

    dev - implement paw3395 for test

--- app/src/main.c
+++ app/src/main.c
@@ -1,19 +1,24 @@
-/*
- * Copyright (c) 2021 Nordic Semiconductor ASA
- * SPDX-License-Identifier: Apache-2.0
- */
-
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
-#include <stdio.h>
+#include <zephyr/sys/printk.h>
 
+int main(void) {
+    const struct device *paw3395 = DEVICE_DT_GET_ONE(pixart_paw3395);
+    if (!device_is_ready(paw3395)) {
+        printk("PAW3395 device not ready\n");
+        return 0;
+    }
 
-int main(void)
-{
-	
-	while (1) {
-
-		k_msleep(5000);
-	}
+    struct sensor_value dx, dy;
+    while (1) {
+        if (sensor_sample_fetch(paw3395) == 0) {
+            sensor_channel_get(paw3395, SENSOR_CHAN_POS_DX, &dx);
+            sensor_channel_get(paw3395, SENSOR_CHAN_POS_DY, &dy);
+            printk("dx: %d, dy: %d\n", dx.val1, dy.val1);
+        }
+        k_msleep(10000);
+    }
 
 	return 0;
 }
