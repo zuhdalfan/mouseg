c3d6b348e8fc3bbe5d04dc22218487b0c5735b08
zuhdi <m.zuhdi@willowmore.com.sg>
2025-07-14 07:43:51 +0700

    dev - add notes in main.c

--- app/src/main.c
+++ app/src/main.c
@@ -7,6 +7,10 @@ int main(void) {
 	k_msleep(1000);
 
     while (1) {
+        // threads created on business_logic.c
+        // polling thread - to get all data and send to host
+        // cursor thread - to get dx dy data from paw3395 sensor
+        // dpi control thread - to check dpi and implement led
         k_msleep(1000);    
     }
 
