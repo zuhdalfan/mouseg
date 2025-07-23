357e8c6658fe0ca7d929d472699c78682428e012
zuhdi <m.zuhdi@willowmore.com.sg>
2025-07-23 19:21:16 +0700

    fix - usb

--- app/src/main.c
+++ app/src/main.c
@@ -1,10 +1,34 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/drivers/uart.h>
 #include <math.h>
+
+BUILD_ASSERT(DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_console), zephyr_cdc_acm_uart),
+	     "Console device is not ACM CDC UART device");
+
+int usb_console(void)
+{
+	const struct device *const dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+	uint32_t dtr = 0;
+
+	if (usb_enable(NULL)) {
+		return 0;
+	}
+
+	/* Poll if the DTR flag was set */
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		/* Give CPU resources to low priority threads. */
+		k_sleep(K_MSEC(100));
+	}
+}
 
 int main(void) {
 
 	k_msleep(1000);
+
+    usb_console();
 
     while (1) {
         // threads created on business_logic.c
